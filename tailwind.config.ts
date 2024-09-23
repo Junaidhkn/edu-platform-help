381605186f46a5fff9b44d73cf09840d84ec4b55
junaidhkn <junaidhkniazi@gmail.com>
2024-09-23 22:45:05 +0500

    config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import * as tailwindcssAnimate from 'tailwindcss-animate';
 
 const config = {
 	darkMode: ['class'],
@@ -74,7 +75,7 @@ const config = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [tailwindcssAnimate],
 } satisfies Config;
 
 export default config;
