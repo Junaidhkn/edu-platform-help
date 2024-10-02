9300de6fa708fac0b1d6734009827c47bca2d8c7
junaidhkn <junaidhkniazi@gmail.com>
2024-10-03 00:44:18 +0500

    commit

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,6 +1,4 @@
 export { default as category } from './category';
-export { default as comment, commentRelations } from './comment';
-export { default as user, userRelations } from './customer';
+export { default as customer, customerRelations } from './customer';
 export { default as orderStatus, orderStatusRelations } from './orderStatus';
 export { default as order, orderRelations } from './order';
-export { default as statusCatalog } from './statusCatalog';
