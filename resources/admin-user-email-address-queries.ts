4fd3ef83d632de1c70bf0a171786dd0a7d7485bd
junaidhkn <junaidhkniazi@gmail.com>
2024-11-30 21:42:44 +0500

    bug fixed

--- resources/admin-user-email-address-queries.ts
+++ resources/admin-user-email-address-queries.ts
@@ -1,11 +1,11 @@
-import "server-only";
-import db from "@/drizzle";
-import { adminUserEmailAddresses, lower } from "@/drizzle/schema";
+import 'server-only';
+import db from '@/src/db';
+import { adminUserEmailAddresses, lower } from '@/src/db/schema/user';
 
 export const findAdminUserEmailAddresses = async () => {
-  const adminUserEmailAddress = await db
-    .select({ email: lower(adminUserEmailAddresses.email) })
-    .from(adminUserEmailAddresses);
+	const adminUserEmailAddress = await db
+		.select({ email: lower(adminUserEmailAddresses.email) })
+		.from(adminUserEmailAddresses);
 
-  return adminUserEmailAddress.map((item) => item.email as string);
+	return adminUserEmailAddress.map((item) => item.email as string);
 };
--- resources/user-queries.ts
+++ resources/user-queries.ts
@@ -1,46 +1,47 @@
-import "server-only";
+import 'server-only';
 
-import db from "@/drizzle";
-import { lower, users } from "@/drizzle/schema";
+import db from '@/src/db';
+import { lower } from '@/src/db/schema/user';
+import { users } from '@/src/db/schema';
 import {
-  desc,
-  eq,
-  getTableColumns,
-  //  getTableColumns
-} from "drizzle-orm";
-import { USER_ROLES } from "@/lib/constants";
-import { auth } from "@/auth";
+	desc,
+	eq,
+	getTableColumns,
+	//  getTableColumns
+} from 'drizzle-orm';
+import { USER_ROLES } from '@/lib/constants';
+import { auth } from '@/auth';
 // import { auth } from "@/auth";
 
 /* ADMIN QUERIES - THESE QUERIES REQUIRE ADMIN ACCESS */
 export async function findAllUsers() {
-  const session = await auth();
+	const session = await auth();
 
-  if (session?.user?.role !== USER_ROLES.ADMIN) {
-    throw new Error("Unauthorized");
-  }
+	if (session?.user?.role !== USER_ROLES.ADMIN) {
+		throw new Error('Unauthorized');
+	}
 
-  const { password, ...rest } = getTableColumns(users);
+	const { password, ...rest } = getTableColumns(users);
 
-  const allUsers = await db
-    .select({ ...rest })
-    .from(users)
-    .orderBy(desc(users.role));
+	const allUsers = await db
+		.select({ ...rest })
+		.from(users)
+		.orderBy(desc(users.role));
 
-  return allUsers;
+	return allUsers;
 }
 /* -------------------------------------------------- */
 
 export const findUserByEmail = async (
-  email: string,
+	email: string,
 ): Promise<typeof users.$inferSelect | null> => {
-  const user = await db
-    .select()
-    .from(users)
-    .where(eq(lower(users.email), email.toLowerCase()))
-    .then((res) => res[0] ?? null);
+	const user = await db
+		.select()
+		.from(users)
+		.where(eq(lower(users.email), email.toLowerCase()))
+		.then((res) => res[0] ?? null);
 
-  return user;
+	return user;
 };
 
 // type UserWithoutPassword = Omit<typeof users.$inferSelect, "password">;
