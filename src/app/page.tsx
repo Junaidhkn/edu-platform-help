13fc67892fa5a054b8a643a382e67771433d1241
junaidhkn <junaidhkniazi@gmail.com>
2024-12-09 21:21:30 +0500

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-const HomePage = () => {
-	return <div>HomePage</div>;
-};
+import NavBar from '@/components/common/NavBar';
+import CaseStudySection from '@/components/sections/CaseStudySection';
+import ContactUsSection from '@/components/sections/ContactUsSection';
+import FooterSection from '@/components/sections/FooterSection';
+import HeroSection from '@/components/sections/HeroSection';
+import LetMakeThingsHappenSection from '@/components/sections/LetMakeThingsHappenSection';
+import LogoGroupSection from '@/components/sections/LogoGroupSection';
+import OurWorkingProcessSection from '@/components/sections/OurWorkingProcessSection';
+import ServiceSection from '@/components/sections/ServiceSection';
+import TeamSection from '@/components/sections/TeamSection';
 
-export default HomePage;
+export default function Home() {
+	return (
+		<main className='flex justify-center items-center min-h-screen'>
+			<div className='w-full max-w-7xl mx-4 md:mx-[100px] pt-[20px] flex flex-col gap-[140px]'>
+				<NavBar />
+				<HeroSection />
+				<LogoGroupSection />
+				<ServiceSection />
+				<LetMakeThingsHappenSection />
+				<CaseStudySection />
+				<OurWorkingProcessSection />
+				<TeamSection />
+				<ContactUsSection />
+				<FooterSection />
+			</div>
+		</main>
+	);
+}
