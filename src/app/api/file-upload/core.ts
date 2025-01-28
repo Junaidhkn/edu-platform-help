338ddf89234427c24451d2ed23aab6facc0f90f1
junaidhkn <junaidhkniazi@gmail.com>
2025-01-29 00:22:57 +0500

    route configs

--- src/app/api/file-upload/core.ts
+++ src/app/api/file-upload/core.ts
@@ -7,25 +7,105 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
 	// Define as many FileRoutes as you like, each with a unique routeSlug
-	fileUploader: f({
+	imageUploader: f({
 		image: {
 			maxFileSize: '128MB',
 			maxFileCount: 20,
 		},
+	})
+		// Set permissions and file types for this FileRoute
+		.middleware(async ({ req }) => {
+			// This code runs on your server before upload
+			const user = await auth();
+
+			// If you throw, the user will not be able to upload
+			if (!user) throw new UploadThingError('Unauthorized');
+
+			// Whatever is returned here is accessible in onUploadComplete as `metadata`
+			return { userId: user.user?.id };
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			// This code RUNS ON YOUR SERVER after upload
+			console.log('Upload complete for userId:', metadata.userId);
+			console.log('file url', file.url);
+			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+			return { uploadedBy: metadata.userId };
+		}),
+	profilePictureUploader: f({
+		image: {
+			maxFileSize: '128MB',
+			maxFileCount: 20,
+		},
+	})
+		.middleware(async ({ req }) => {
+			// This code runs on your server before upload
+			const user = await auth();
+
+			// If you throw, the user will not be able to upload
+			if (!user) throw new UploadThingError('Unauthorized');
+
+			// Whatever is returned here is accessible in onUploadComplete as `metadata`
+			return { userId: user.user?.id };
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			// This code RUNS ON YOUR SERVER after upload
+			console.log('Upload complete for userId:', metadata.userId);
+			console.log('file url', file.url);
+			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+			return { uploadedBy: metadata.userId };
+		}),
+	pdfUploader: f({
 		pdf: {
-			maxFileSize: '1024MB',
-			maxFileCount: 10,
-		},
-		blob: {
-			maxFileSize: '1GB',
-			maxFileCount: 4,
-		},
-		text: {
-			maxFileSize: '16MB',
+			maxFileSize: '256MB',
 			maxFileCount: 4,
 		},
 	})
-		// Set permissions and file types for this FileRoute
+		.middleware(async ({ req }) => {
+			// This code runs on your server before upload
+			const user = await auth();
+
+			// If you throw, the user will not be able to upload
+			if (!user) throw new UploadThingError('Unauthorized');
+
+			// Whatever is returned here is accessible in onUploadComplete as `metadata`
+			return { userId: user.user?.id };
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			// This code RUNS ON YOUR SERVER after upload
+			console.log('Upload complete for userId:', metadata.userId);
+			console.log('file url', file.url);
+			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+			return { uploadedBy: metadata.userId };
+		}),
+	blobUploader: f({
+		blob: {
+			maxFileSize: '1024MB',
+			maxFileCount: 1,
+		},
+	})
+		.middleware(async ({ req }) => {
+			// This code runs on your server before upload
+			const user = await auth();
+
+			// If you throw, the user will not be able to upload
+			if (!user) throw new UploadThingError('Unauthorized');
+
+			// Whatever is returned here is accessible in onUploadComplete as `metadata`
+			return { userId: user.user?.id };
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			// This code RUNS ON YOUR SERVER after upload
+			console.log('Upload complete for userId:', metadata.userId);
+			console.log('file url', file.url);
+			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+			return { uploadedBy: metadata.userId };
+		}),
+	textUploader: f({
+		text: {
+			maxFileSize: '64MB',
+			maxFileCount: 8,
+		},
+	})
 		.middleware(async ({ req }) => {
 			// This code runs on your server before upload
 			const user = await auth();
