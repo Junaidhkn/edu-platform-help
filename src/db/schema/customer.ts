72bb3e6c130861c8b4d8f882cf449c5860c382ff
junaidhkn <junaidhkniazi@gmail.com>
2025-02-01 15:07:55 +0500

    commit

--- src/db/schema/customer.ts
+++ src/db/schema/customer.ts
@@ -3,12 +3,10 @@ import { pgEnum, pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 
 import order from './order';
 import { randomUUID } from 'crypto';
-const statusEnum = pgEnum('status', ['draft', 'active', 'dilevered']);
-const acedemicLevelEnum = pgEnum('acedemic_level', [
+const academicLevelEnum = pgEnum('academic_level', [
 	'undergraduate',
 	'graduate',
 ]);
-// status: statusEnum('status')
 
 const id = () => {
 	return text('customer_id')
@@ -22,7 +20,7 @@ const customer = pgTable('customers', {
 	lastName: varchar('name', { length: 255 }).notNull(),
 	phone: varchar('contact_phone', { length: 255 }).notNull().unique(),
 	email: varchar('email', { length: 255 }).notNull().unique(),
-	acedemicLevel: acedemicLevelEnum('status'),
+	academicLevel: academicLevelEnum('status'),
 	password: varchar('password', { length: 255 }).notNull(),
 	createdAt: timestamp('created_at', { mode: 'string' }).notNull().defaultNow(),
 	updatedAt: timestamp('updated_at', { mode: 'string' }).notNull().defaultNow(),
