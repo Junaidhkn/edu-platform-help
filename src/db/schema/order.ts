8b9d2eab84ff33f3f004557c6ede59762a6996b5
junaidhkn <junaidhkniazi@gmail.com>
2025-02-15 21:31:33 +0500

    commit

--- src/db/schema/order.ts
+++ src/db/schema/order.ts
@@ -1,11 +1,4 @@
-import {
-	pgTable,
-	serial,
-	timestamp,
-	numeric,
-	text,
-	pgEnum,
-} from 'drizzle-orm/pg-core';
+import { pgTable, timestamp, numeric, text } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
 
 import user from './user';
