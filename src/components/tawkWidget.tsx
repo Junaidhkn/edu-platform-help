a181492bd8a32022123641cae8b560da3b2c253f
junaidhkn <junaidhkniazi@gmail.com>
2025-07-01 02:52:08 +0500

    chat test

--- src/components/tawkWidget.tsx
+++ src/components/tawkWidget.tsx
@@ -7,21 +7,35 @@ const TawkWidget = () => {
 			strategy='lazyOnload'
 			dangerouslySetInnerHTML={{
 				__html: `
-         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://tawk.to/chat/${process.env.propertyId}/${process.env.widgetId}';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();`,
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+ (function(){
+ var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+ s1.async=true;
+ s1.src='https://embed.tawk.to/686300ec319b9019082a5ca4/1iv1cuf3k';
+ s1.charset='UTF-8';
+ s1.setAttribute('crossorigin','*');
+ s0.parentNode.insertBefore(s1,s0);
+ })();`,
 			}}
 		/>
 	);
 };
 
 export default TawkWidget;
+
+// <!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/686300ec319b9019082a5ca4/1iv1cuf3k';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+// </script>
+// <!--End of Tawk.to Script-->
 
 // <!--Start of Tawk.to Script-->
 // <script type="text/javascript">
