5ce408adcd650115029ef00e97a62b083ccb66be
junaidhkn <junaidhkniazi@gmail.com>
2025-07-03 06:38:00 +0500

    chat source updated

--- src/components/tawkWidget.tsx
+++ src/components/tawkWidget.tsx
@@ -13,7 +13,7 @@ const TawkWidget = () => {
  (function(){
  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
  s1.async=true;
- s1.src=https://embed.tawk.to/686300ec319b9019082a5ca4/1iv1cuf3k;
+ s1.src=https://tawk.to/chat/686300ec319b9019082a5ca4/1iv1bn7ir;
  s1.charset='UTF-8';
  s1.setAttribute('crossorigin','*');
  s0.parentNode.insertBefore(s1,s0);
